d43f6c392d725d3928a68c99ecde28de13808fb1
wessport <wesporter92@gmail.com>
2016-06-10 17:23:59 -0400

    DEBUGGING. GRAWR.

--- Tutorial_Shiny_App.R
+++ Tutorial_Shiny_App.R
@@ -7,6 +7,7 @@
 #    http://shiny.rstudio.com/
 #
 
+
 library(shiny)
 
 # UI
@@ -16,49 +17,53 @@ ui <- shinyUI(fluidPage(
   
   # Application title
   titlePanel("My Shiny App"),
-  
+
   headerPanel("File input test"),
-  
-  # Sidebar  
-  sidebarLayout(sidebarPanel(
-    fileInput( #fileInput() function is used to upload the file of interest
-      "file",
-      label = "File Input",
-      multiple = FALSE,
-      accept = NULL,
-      width = NULL
-    ),
-    helpText("Default max. file size is 5 MB")
-  ), 
-  
-  # Main panel - plots go here
-  mainPanel(tableOutput("tb"))
+
+#   # Sidebar
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file", label = "File Input", multiple = FALSE),
+      helpText("Default max. file size is 5 MB"),
+      tags$hr(),
+      h5(helpText("Does your data have headers?")),
+       checkboxInput(inputId = 'header', label = 'Header', value = FALSE),
+      br(),
+
+radioButtons(inputId = 'sep', label = 'Separator', choices = c(Comma=',',Semicolon=';',Tab='\t', Space=''), selected = ',')),
+
+# Main panel - plots go here
+    mainPanel(
+          uiOutput("tb")
+    )
+
   )
 ))
+
+
 
 #SERVER
 
 # Define server logic required to output table
 server <- shinyServer(function(input, output) {
-  
-  
-  
   #Converting the csv file to a dataframe
   
   data <- reactive({
     #Assigning the file input to file1 so we can get the datapath for read.table
     file1 <- input$file
     
-    if (is.null(input$file)) {
+    if (is.null(file1)) {
       # If user has not uploaded a file yet don't do anything
       return(NULL)
     }
     else
       
-      (read.table(file1$datapath,
-                  header = F,
-                  ",",
-                  stringsAsFactors = F))
+      (read.table(
+        file1$datapath,
+        header = input$header,
+        sep = input$sep,
+        stringsAsFactors = F
+      ))
     
   })
   
@@ -78,10 +83,10 @@ server <- shinyServer(function(input, output) {
   #This displays a table of the data held in the uploaded file
   output$dataTable <- renderTable({
     if (is.null(data())) {
-    return(NULL)
-  } #else
-  
-  data()
+      return(NULL)
+    } #else
+    
+    data()
     
   })
   
@@ -100,7 +105,7 @@ server <- shinyServer(function(input, output) {
   output$tb <- renderUI({
     if (is.null(data()))
       
-      h5("Feed me data")
+      h5("FEED ME DATA")
     
     else
       
@@ -115,5 +120,5 @@ server <- shinyServer(function(input, output) {
 })
 
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
