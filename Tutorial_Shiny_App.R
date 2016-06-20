82c1db98bc0256fbee03ca644e097b5577ac73be
wessport <wesporter92@gmail.com>
2016-06-20 15:19:16 -0400

    Trying to add example capability.

--- Tutorial_Shiny_App.R
+++ Tutorial_Shiny_App.R
@@ -21,71 +21,77 @@ library(shinythemes)
 # Define UI for application 
 ui <- shinyUI(fluidPage( theme = shinytheme("united"),
                          
-                         # Application title
-                         titlePanel(h1("Langmuir Shiny App", align = "center")),
+ # Application title
+   titlePanel(h1("Langmuir Shiny App", align = "center")),
                          
-                         headerPanel(h3("Import Data")),
+    headerPanel(h3("Import Data")),
                          
-                         # Sidebar
-                         sidebarLayout(
-                           sidebarPanel(
-                             fileInput("file", label = "File Input", multiple = FALSE),
-                             helpText("CSV files only. Default max.file size is 5 MB."),
-                             tags$hr(),
-                             h5(("Does your data have headers?")),
-                             checkboxInput(inputId = 'header', label = 'Header', value = FALSE),
-                             br(),
-                             
-                             radioButtons(inputId = 'sep', label = 'Separator', choices = c(Comma=',',Semicolon=';',Tab='\t', Space=''), selected = ',')),
-                           
-                           # Main panel - plots go here
-                           mainPanel(
-                             
-                             # This is a conditonal statment which only displays 
-                             # the landing page if a file has yet to be uploaded.
-                             conditionalPanel("output.fileUploaded", 
-                                              
-                                              h3("Welcome!", style = "color:blue"),
-                                              br(),
-                                              p("This app is intended to facilitate the generation
-                                                of sorption isotherms and to fit sorption data
-                                                to the Langmuir equation. It can also be used to
-                                                estimate parameter values such as maximum sorption
-                                                and binding affinity."),
-                                              br(),
-                                              p("Begin by uploading a", strong("csv"), "file
-                                                of your dataset to the left. If your dataset has headers
-                                                be sure to check the header option. If uploading a csv
-                                                file that is", strong("not"), "comma separated, ensure to check
-                                                the appropriate delimiter option as well."),
-                                              br(),
-                                              p("To return to this page, simply reload your web browser.")
-                                              ),
-                             
-                             conditionalPanel("output.dataReady", 
-                                              tabsetPanel(
-                                                tabPanel(inputId="tab1", "Plot", 
-                                                         textInput(inputId="title", label = "Write a Title", value = "Sorption Isotherm"), 
-                                                         textInput(inputId="xTitle", label = "X-Axis Label", value = "Equilibrium Conc. mg/L"),
-                                                         textInput(inputId="yTitle", label = "Y-Axis Label", value = "Sorbed mg/kg"),
-                                                         plotOutput("graph"),
-                                                         sliderInput("yAxis", "Adjust Y-Axis", 0, 10000, 4000),
-                                                         checkboxInput(inputId = 'logTrans', label = 'Log Transform Y-Axis', value = FALSE),
-                                                         textOutput("Qmax"), 
-                                                         textOutput("k"),
-                                                         textOutput("E"),
-                                                         downloadButton("downloadPlot", "Download Plot as PNG")),
-                                                tabPanel(inputId="tab2", "Residuals of Fit", plotOutput("graphResid")),
-                                                tabPanel(inputId="tab3",  "Data", tableOutput("dataTable")),
-                                                tabPanel(inputId="tab4", "Data Summary", tableOutput("sum")),
-                                                tabPanel(inputId="tab5", "File Info", tableOutput("filedf"))
-                                                
-                                                
-                                                
-                                              )
-                             )
-                                              )))
-                         )
+ # Sidebar
+ sidebarLayout(
+   sidebarPanel(
+     fileInput("file", label = "File Input", multiple = FALSE),
+     helpText("CSV files only. Default max.file size is 5 MB."),
+     tags$hr(),
+     h5(("Does your data have headers?")),
+     checkboxInput(inputId = 'header', label = 'Header', value = FALSE),
+     br(),
+     
+     radioButtons(inputId = 'sep', label = 'Separator', choices = c(Comma=',',Semicolon=';',Tab='\t', Space=''), selected = ',')),
+   
+   # Main panel - plots go here
+   mainPanel(
+     
+     # This is a conditonal statment which only displays 
+     # the landing page if a file has yet to be uploaded.
+     conditionalPanel("output.fileUploaded", 
+                      
+                      h3("Welcome!", style = "color:blue"),
+                      br(),
+                      p("This app is intended to facilitate the generation
+                         of sorption isotherms and to fit sorption data
+                         to the Langmuir equation. It can also be used to
+                         estimate parameter values such as maximum sorption
+                         and binding affinity."),
+                      br(),
+                      h4("Instructions:", style = "color:#e95420"),
+                      p("Begin by uploading a", strong("csv"), "file
+                         of your dataset to the left. If your dataset has headers
+                         be sure to check the header option. If uploading a csv
+                         file that is", strong("not"), "comma separated, ensure to check
+                         the appropriate delimiter option as well."),
+                      br(),
+                      p("Click the button below to try out an example!"),
+                      br(),
+                      actionButton("example", "Example Sorption Data"),
+                      verbatimTextOutput("test"),
+                      br(),
+                      br(),
+                      p("To return to this page, simply reload your web browser.")),
+     
+     conditionalPanel("output.dataReady", 
+                      tabsetPanel(
+                        tabPanel(inputId="tab1", "Plot", 
+                                 textInput(inputId="title", label = "Write a Title", value = "Sorption Isotherm"), 
+                                 textInput(inputId="xTitle", label = "X-Axis Label", value = "Equilibrium Conc. mg/L"),
+                                 textInput(inputId="yTitle", label = "Y-Axis Label", value = "Sorbed mg/kg"),
+                                 plotOutput("graph"),
+                                 sliderInput("yAxis", "Adjust Y-Axis", 0, 10000, 4000),
+                                 checkboxInput(inputId = 'logTrans', label = 'Log Transform Y-Axis', value = FALSE),
+                                 textOutput("Qmax"), 
+                                 textOutput("k"),
+                                 textOutput("E"),
+                                 downloadButton("downloadPlot", "Download Plot as PNG")),
+                        tabPanel(inputId="tab2", "Residuals of Fit", plotOutput("graphResid")),
+                        tabPanel(inputId="tab3",  "Data", tableOutput("dataTable")),
+                        tabPanel(inputId="tab4", "Data Summary", tableOutput("sum")),
+                        tabPanel(inputId="tab5", "File Info", tableOutput("filedf"))
+                        
+                                )
+                      )
+        )
+      )
+     )
+  )
 
 
 
@@ -94,17 +100,44 @@ ui <- shinyUI(fluidPage( theme = shinytheme("united"),
 # Define server logic required to output table
 server <- shinyServer(function(input, output) {
   
+  # Example Data
+  exampleData <- reactive({
+      
+      X <- c(1.80,1.80,1.80,3.18,3.18,5.01,5.01,6.68,6.68,9.55,9.55,50.20,50.20,97.50,97.50,153.00,153.00,204.00,204.00,309.00,309.00)
+    
+      Y <- c(-8.412862,-2.097072,2.097072,15.078833,14.131693,34.210901,32.465827,53.999099,54.224393,78.055991,78.148600,376.304746,362.922643,538.938132,529.046422,690.271349,688.964188,816.090450,768.636319,838.989736,866.996974)
+    
+      exampleData <- data.frame(X,Y)
+    
+  })
+
+  test1 <- reactive({1})
   
-  #Converting the csv file to a dataframe
+  test2 <- eventReactive(input$example, {1})
+  
+  test3 <- reactive({test1()+test2()})
+  
+  output$test <- reactive({test3()})
+  
+  #Converting the uploaded csv file to a dataframe
   data <- reactive({
     
     #Assigning the file input to file1 so we can get the datapath for read.table
-    file1 <- input$file
+  
+    file1<- if( 1 == 1){input$file}
     
     if (is.null(file1)) {
       
       return(NULL)
-    } 
+    
+      } else {
+      
+      #file1<- if(exampleCalled !=1) {input$file} else{1}
+        
+      if(file1 == 1) {myData <- exampleData()} else {
+      
+      
+      
     if (input$header == F){
       isolate({ 
         myData <- read.table(file1$datapath,header = input$header,sep = input$sep,stringsAsFactors = F) 
@@ -113,7 +146,13 @@ server <- shinyServer(function(input, output) {
         colnames(myData) <- c("X","Y")
         
       })
-      myData} else {read.table(file1$datapath,header = input$header,sep = input$sep,stringsAsFactors = F)}
+      myData} else {myData <- read.table(file1$datapath,header = input$header,sep = input$sep,stringsAsFactors = F)
+        
+                    colnames(myData) <- c("X","Y")
+      
+                    myData }  } }
+    
+    
   })
   
   
